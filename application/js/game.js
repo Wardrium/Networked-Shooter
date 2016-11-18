c8ef6594d107576736769d3587c70a7cd43e8da5
Ward <wardrium@gmail.com>
2016-11-17 23:56:33 -0500

    After setting name, game will display a circle that can be controlled with arrow keys.

--- application/js/game.js
+++ application/js/game.js
@@ -8,8 +8,8 @@ var MenuLayer = cc.Layer.extend({
         var size = cc.director.getWinSize();
 
         var background = new cc.DrawNode();
-        background.drawRect(cc.p(0, 0), cc.p(960, 640), cc.color(0, 0, 0, 255));
-        this.addChild(background, 0)
+        background.drawRect(cc.p(0, 0), cc.p(size.width, size.height), cc.color(0, 0, 0, 255));
+        this.addChild(background, 0);
 
         var textInput = ccui.TextField.create("Enter Your Name", "Arial", 40);
         textInput.setTextHorizontalAlignment(cc.TEXT_ALIGNMENT_CENTER);
@@ -24,6 +24,7 @@ var MenuLayer = cc.Layer.extend({
                     case cc.KEY.enter:
                         console.log('Enter pressed. Sending request...');
                         sc.send_request(JSON.stringify({name: textInput.string}));
+                        cc.director.runScene(GameLayer.scene(textInput.string));
                         break;
                 }
             }
@@ -41,31 +42,63 @@ MenuLayer.scene = function(){
 
 
 var GameLayer = cc.Layer.extend({
-    ctor:function(){
+    ctor:function(name){
         this._super();
-        this.init();
+        this.init(name);
     },
-    init:function(){
+    init:function(name){
         this._super();
+        
         var size = cc.director.getWinSize();
 
-        var sprite = cc.Sprite.create("HelloWorld.png");
+        var background = new cc.DrawNode();
+        background.drawRect(cc.p(0, 0), cc.p(size.width, size.height), cc.color(0, 0, 0, 255));
+        this.addChild(background, 0);
+
+        var player = new cc.DrawNode();
+        player.setPosition(cc.p(size.width / 2, size.height / 2));
+        player.drawCircle(cc.p(0, 0), 50, 360, 50, false, 4, cc.color(255, 0, 0, 255));
+        this.addChild(player, 1);
+
+        var nameTag = cc.LabelTTF.create(name, "Arial", 20);
+        player.addChild(nameTag, 1);
+
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed:function(key, event){
+                switch(key){
+                    case cc.KEY.left:
+                        player.setPosition(cc.p(player.getPositionX() - 5, player.getPositionY()));
+                        break;
+                    case cc.KEY.right:
+                        player.setPosition(cc.p(player.getPositionX() + 5, player.getPositionY()));
+                        break;
+                    case cc.KEY.up:
+                        player.setPosition(cc.p(player.getPositionX(), player.getPositionY() + 5));
+                        break;
+                    case cc.KEY.down:
+                        player.setPosition(cc.p(player.getPositionX(), player.getPositionY() - 5));
+                        break;
+
+                }
+            }
+        }, this);
+
+        /*var sprite = cc.Sprite.create("HelloWorld.png");
         sprite.setPosition(size.width / 2, size.height / 2);
         sprite.setScale(1.0);
-        this.addChild(sprite, 0);
+        this.addChild(sprite, 0);*/
 
-        var label = cc.LabelTTF.create("Hello World", "Arial", 40);
-        label.setPosition(size.width / 2, size.height / 2);
-        this.addChild(label, 1);
+
     },
     onEnter:function(){
         this._super();
     }
 });
 
-GameLayer.scene = function(){
+GameLayer.scene = function(name){
     var scene = new cc.Scene();
-    var layer = new GameLayer();
+    var layer = new GameLayer(name);
     scene.addChild(layer);
     return scene;
 }
